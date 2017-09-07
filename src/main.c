d0378839d2032c74b14b0725c8f4f85d6a7d1311
RuStrannik <31518594+RuStrannik@users.noreply.github.com>
2017-09-06 20:40:24 -0400

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,20 +1,27 @@
 /*
 
-Recommended library folder structure
+Recommended library folder structure:
 
 STM32/library
  ├─CMSIS
- │  ├─Device/ST/STM32F4xx/
- │  │  ├─Include
- │  │  └─Source/Templates/gcc_ride7
+ │  ├─Device
+ │  │  └─ST
+ │  │    └─STM32F4xx
+ │  │      ├─Include
+ │  │      └─Source
+ │  │        └─Templates
+ │  │          └─gcc_ride7
  │  └─Include
  ├─FreeRTOS_9.0.0
  │  ├─include
- │  ├─MemMang
- │  └─MPU
+ │  └─portable
+ │    ├─Common
+ │    ├─GCC
+ │    │  └─ARM_CM4F
+ │    └─MemMang
  └─STM32F4xx_StdPeriph_Driver
-	├─inc
-	└─src
+   ├─inc
+   └─src
 */
 
 
